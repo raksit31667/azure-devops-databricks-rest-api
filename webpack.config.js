c3e6b1194f4d29e88f270e7de844eb9b021d56d2
Raksit <raksit.m@ku.th>
2020-09-21 11:12:07 +0700

    🔨 configure webpack for multi-task building

--- webpack.config.js
+++ webpack.config.js
@@ -25,87 +25,90 @@ module.exports = env => {
         process.exit(1);
     }
 
-    const config = {
+    const tasks = ['verify-all-jobs-running', 'update-instance-pools'];
+    return tasks.map(task => {
+        const config = {
 
-        entry: {
-            "main": "./src/verify-all-jobs-running/main.ts",
-        },
-
-        plugins: [
-            new CopyWebpackPlugin([
-                // These files are needed by azure-pipelines-task-lib library.
-                {
-                    from: path.resolve("./node_modules/azure-pipelines-task-lib/lib.json"),
-                    to: path.join(Target, "verify-all-jobs-running")
-                },
-                {
-                    from: path.resolve("./node_modules/azure-pipelines-task-lib/Strings"),
-                    to: path.join(Target, "verify-all-jobs-running")
-                },
-
-                {
-                    from: path.join(__dirname, "./src/verify-all-jobs-running/task.json"),
-                    to: path.join(Target, "verify-all-jobs-running")
-                },
-                {
-                    from: path.join(__dirname, "./images/icon.png"),
-                    to: path.join(Target, "verify-all-jobs-running", "icon.png")
-                },
-                {
-                    from: path.join(__dirname, "./manifests/base.json"),
-                    to: Target
-                },
-                {
-                    from: path.join(__dirname, "./manifests", `${env}.json`),
-                    to: Target
-                },
-                {
-                    from: path.join(__dirname, "./images/icon.png"),
-                    to: Target
-                },
-                {
-                    from: path.join(__dirname, "./src/README.md"),
-                    to: Target
-                }
-            ]),
-
-            WebpackCommon.PackageJsonLoadFixer(Target, [
-                "verify-all-jobs-running/main.js",
-            ]),
-
-            WebpackCommon.VersionStringReplacer(Target, [
-                "verify-all-jobs-running/task.json",
-                "base.json"
-            ]),
-
-            new ReplaceInFileWebpackPlugin([
-                {
-                    dir: Target,
-                    files: [
-                        "verify-all-jobs-running/main.js",
-                        "verify-all-jobs-running/task.json",
-                        "base.json"
-                    ],
-                    rules: [
-                        // This replacement is required to allow azure-pipelines-task-lib to load the 
-                        // json resource file correctly
-                        {
-                            search: /__webpack_require__\(.*\)\(resourceFile\)/,
-                            replace: 'require(resourceFile)'
-                        },
-                        {
-                            search: /{{taskid}}/ig,
-                            replace: Settings[env].TaskGuid
-                        },
-                        {
-                            search: /{{tag}}/ig,
-                            replace: Settings[env].Tag
-                        }
-                    ]
-                }
-            ])
-        ],
-    };
-
-    return WebpackCommon.FillDefaultNodeSettings(config, env, "verify-all-jobs-running");
+            entry: {
+                "main": `./src/${task}/main.ts`,
+            },
+    
+            plugins: [
+                new CopyWebpackPlugin([
+                    // These files are needed by azure-pipelines-task-lib library.
+                    {
+                        from: path.resolve("./node_modules/azure-pipelines-task-lib/lib.json"),
+                        to: path.join(Target, task)
+                    },
+                    {
+                        from: path.resolve("./node_modules/azure-pipelines-task-lib/Strings"),
+                        to: path.join(Target, task)
+                    },
+    
+                    {
+                        from: path.join(__dirname, `./src/${task}/task.json`),
+                        to: path.join(Target, task)
+                    },
+                    {
+                        from: path.join(__dirname, "./images/icon.png"),
+                        to: path.join(Target, task, "icon.png")
+                    },
+                    {
+                        from: path.join(__dirname, "./manifests/base.json"),
+                        to: Target
+                    },
+                    {
+                        from: path.join(__dirname, "./manifests", `${env}.json`),
+                        to: Target
+                    },
+                    {
+                        from: path.join(__dirname, "./images/icon.png"),
+                        to: Target
+                    },
+                    {
+                        from: path.join(__dirname, "./src/README.md"),
+                        to: Target
+                    }
+                ]),
+    
+                WebpackCommon.PackageJsonLoadFixer(Target, [
+                    `${task}/main.js`,
+                ]),
+    
+                WebpackCommon.VersionStringReplacer(Target, [
+                    `${task}/task.json`,
+                    "base.json"
+                ]),
+    
+                new ReplaceInFileWebpackPlugin([
+                    {
+                        dir: Target,
+                        files: [
+                            `${task}/main.js`,
+                            `${task}/task.json`,
+                            "base.json"
+                        ],
+                        rules: [
+                            // This replacement is required to allow azure-pipelines-task-lib to load the 
+                            // json resource file correctly
+                            {
+                                search: /__webpack_require__\(.*\)\(resourceFile\)/,
+                                replace: 'require(resourceFile)'
+                            },
+                            {
+                                search: /{{taskid}}/ig,
+                                replace: Settings[env].TaskGuid
+                            },
+                            {
+                                search: /{{tag}}/ig,
+                                replace: Settings[env].Tag
+                            }
+                        ]
+                    }
+                ])
+            ],
+        };
+    
+        return WebpackCommon.FillDefaultNodeSettings(config, env, task);
+    });
 };
